f71e850d27a9a7d07f9ec5b2d79bc9c922db1545
AysuAnar <8921@holbertonstudents.com>
2024-06-25 16:46:54 +0000

    adding space to line1

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
---Create a table named first_table if not exists with one column id of type INT and a column name of type VARCHAR(256).
+-- Create a table named first_table if not exists with one column id of type INT and a column name of type VARCHAR(256).
 CREATE TABLE IF NOT EXISTS `first_table` (
 	`id` INT,
 	`name` VARCHAR(256)
